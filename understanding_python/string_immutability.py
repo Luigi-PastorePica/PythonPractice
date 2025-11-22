e4f66ebd44b50e576167087f0028255685e74bf3
Luigi Pastore <luigi.pastorepica@outlook.com>
2025-11-21 20:52:30 -0500

    Adds example of string immutability.

---
+++ understanding_python/string_immutability.py
@@ -0,0 +1,28 @@
+from timeit import timeit
+
+def append_text() -> str:
+	text: str = ""
+	for i in range(100):
+		text += 'text'
+	return text
+
+def join_text() -> str:
+	elements: list[str]= []
+	for i in range(100):
+		elements.append("text")
+	return "".join(elements)
+
+
+def main():
+	print(f"Same result? {append_text() == join_text()}")
+	warm_up: float = timeit(append_text)
+
+	append_time: float = timeit(append_text)
+	join_time: float = timeit(join_text)
+
+	print(f"Warm up result: {warm_up:.5f}s") # Sample result: 2.36116s
+	print(f"Append result: {append_time:.5f}s") # Sample result: 2.36533s
+	print(f"Join result: {join_time:.5f}s") # Sample result: 2.13091s
+
+if __name__ == "__main__":
+	main()
